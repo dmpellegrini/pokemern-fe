bde5da5a19558e343cb84fe763785a976fff00f6
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-03-12 21:56:25 -0400

    Added sprites to home website

---
+++ src/sprite/Sprite.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sprite.css"
+
+function Sprite () {
+  return (
+    <div className="Sprite">
+      <img className="pokemon-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="bulbasaur"/>
+      <h3 className="pokemon-title">Bulbasaur</h3>
+
+    </div>
+  )
+}
+
+export default Sprite 
