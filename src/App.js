3c8854f5faa74400ecd799138c6a8e41b25792cd
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-03-12 21:57:48 -0400

    Add sprites to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './navbar/Navbar';
+import Home from './home/Home';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Home />
     </div>
   );
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Sprite from "../sprite/Sprite";
 import "./home.css"
 
 function Home () {
   return (
     <div className="Home">
+      <div className="home-button-container">
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+        <Sprite />
+      </div>
 
     </div>
   )
