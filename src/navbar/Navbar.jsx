e58b01a246771ddd35a6e1c0b6fd005a9234304b
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-03-12 21:54:36 -0400

    Added Github link to navbar items

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -4,20 +4,25 @@ import "./navbar.css";
 function Navbar() {
   return (
     <div className="Navbar">
-      <h1 className="navbar-title">PoKéMern</h1>
-      <ul className="navbar-list">
-        <li className="navbar-list-item" >
-          Home
-        </li>
-        <li className="navbar-list-item" >
-          Search
-        </li>
-        <li className="navbar-list-item" >
-          <a href="https://github.com/dmpellegrini/pokemern-fe" target="_blank">
-            Github
-          </a>
-        </li>
-      </ul>
+      <div className="navbar-spacer">
+        <h1 className="navbar-spacer-title">PoKéMern</h1>
+      </div>
+      <div className="navbar-container">
+        <h1 className="navbar-title">PoKéMern</h1>
+        <ul className="navbar-list">
+          <li className="navbar-list-item" >
+            Home
+          </li>
+          <li className="navbar-list-item" >
+            Search
+          </li>
+          <li className="navbar-list-item" >
+            <a href="https://github.com/dmpellegrini/pokemern-fe" target="_blank">
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
 
   );
